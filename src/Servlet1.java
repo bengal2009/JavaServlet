0ca22068efb96c03a4f9f292154377a94594c331
bengal2009 <bennylin@outlook.com>
2015-05-20 16:49:18 +0800

    V1

---
+++ src/Servlet1.java
@@ -0,0 +1,30 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Lin on 2015/5/20.
+ */
+@WebServlet(name = "Servlet1")
+public class Servlet1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1> Hello! World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+}
