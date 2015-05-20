c2600aac5f304e2e6d8d745734d87ef245968630
bengal2009 <bennylin@outlook.com>
2015-05-20 16:24:19 +0800

    V1

--- src/json/com/MySqlPerson.java
+++ src/json/com/MySqlPerson.java
@@ -3,25 +3,70 @@ package json.com;
 /**
  * Created by Lin on 2015/5/16.
  */
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.*;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 public class MySqlPerson extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        firstMethod(req,resp);
+//        firstMethod(req, resp);
+        firstMethod(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doGet(req, resp);
     }
+    public void SecondMethod(HttpServletRequest request,
+                            HttpServletResponse response) {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        JSONObject json      = new JSONObject();
+        JSONArray addresses = new JSONArray();
+        JSONObject address;
+        try
+        {
+            int count = 15;
 
+            for (int i=0 ; i<count ; i++)
+            {
+                address = new JSONObject();
+                address.put("CustomerName"     , "Decepticons" + i);
+                address.put("AccountId"        , "1999" + i);
+                address.put("SiteId"           , "1888" + i);
+                address.put("Number"            , "7" + i);
+                address.put("Building"          , "StarScream Skyscraper" + i);
+                address.put("Street"            , "Devestator Avenue" + i);
+                address.put("City"              , "Megatron City" + i);
+                address.put("ZipCode"          , "ZZ00 XX1" + i);
+                address.put("Country"           , "CyberTron" + i);
+                addresses.add(address);
+            }
+            json.put("Addresses", addresses);
+//            response.setContentType("application/json");
+
+            response.getWriter().write(json.toString());
+        }
+
+        catch (Exception E)
+        {
+
+        }
+
+
+    }
     public void firstMethod(HttpServletRequest request,
                             HttpServletResponse response) {
         response.setContentType("text/html");
